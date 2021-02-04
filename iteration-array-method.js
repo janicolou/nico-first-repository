2ac637f2dbaffc9d6f9c4b0b7e62cfdfe456ffba
Nico Gabo <gabo.janicolo@gmail.com>
2021-02-05 01:00:05 +0800

    another commit

--- iteration-array-method.js
+++ iteration-array-method.js
@@ -1,5 +1,12 @@
 const {clear} = require('console');
-const fetch = require('node-fetch');
+//const fetch = require('node-fetch');
 const personData = require('./sample-data.json');
 
-console.log(personData);
+//console.log(personData);
+personData.forEach(element => {
+    console.log(element.name);
+});
+
+for (x in personData.age){
+    console.log(x);
+}
