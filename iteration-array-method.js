8403f0efda36b307ef52a5739e1c3315904106ab
Nico Gabo <gabo.janicolo@gmail.com>
2021-02-07 19:55:49 +0800

    medyo ayus na

--- iteration-array-method.js
+++ iteration-array-method.js
@@ -1,12 +1,48 @@
 const {clear} = require('console');
+const { Z_FILTERED } = require('zlib');
 //const fetch = require('node-fetch');
 const personData = require('./sample-data.json');
 
-//console.log(personData);
-personData.forEach(element => {
-    console.log(element.name);
+console.log("\n<------------------------------------------------------->");
+console.log("Printing out all of the data using iteration methods");
+
+console.log("\nfor:");
+for (let i = 0; i < personData.length; i++){
+    console.log(personData[i]);
+}
+
+console.log("\nfor in:");
+for (const i in personData){
+    console.log(personData[i]);
+}
+
+console.log("\nfor of:");
+for(const i of personData){
+    console.log(i);
+}
+
+console.log("\nfor each:");
+personData.forEach(i => {
+    console.log(i);
 });
 
-for (x in personData.age){
-    console.log(x);
+console.log("\nwhile:");
+let i = 0;
+while (i < personData.length){
+    console.log(personData[i]);
+    i++;
 }
+
+console.log("\n<------------------------------------------------------->");
+console.log("Printing out a portion of the data using array methods");
+
+console.log("\nfilter:");
+    console.log(personData.filter (i => i.city === "Malasiqui"));
+
+console.log("\nmap:");
+    console.log(personData.map(i => `${i.name}  ${i.age}` ));
+
+console.log("\nreduce");
+    console.log(personData.reduce((accumulator, currentValue) => accumulator.age + currentValue.age));
+
+console.log("\n<------------------------------------->\n");
