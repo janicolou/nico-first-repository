f78127191d4b12829f7ef297f75a2ba25cde959a
Nico Gabo <gabo.janicolo@gmail.com>
2021-02-08 09:08:49 +0800

    reformatted js file

--- iteration-array-method.js
+++ iteration-array-method.js
@@ -1,40 +1,41 @@
-const personData = require('./sample-data.json');
+const personData = require("./sample-data.json");
 
 console.log("\n<------------------------------------------------------->");
 console.log("Printing out all of the data using iteration methods");
 
 console.log("\nfor:");
-for (let i = 0; i < personData.length; i++)
-    console.log(personData[i]);
+for (let i = 0; i < personData.length; i++) console.log(personData[i]);
 
 console.log("\nfor in:");
-for (const i in personData)
-    console.log(personData[i]);
+for (const i in personData) console.log(personData[i]);
 
 console.log("\nfor of:");
-for(const i of personData)
-    console.log(i);
+for (const i of personData) console.log(i);
 
 console.log("\nfor each:");
-personData.forEach(i => console.log(i));
+personData.forEach((i) => console.log(i));
 
 console.log("\nwhile:");
 let i = 0;
-while (i < personData.length){
-    console.log(personData[i]);
-    i++;
+while (i < personData.length) {
+  console.log(personData[i]);
+  i++;
 }
 
 console.log("\n<------------------------------------------------------->");
 console.log("Printing out a portion of the data using array methods");
 
 console.log("\nfilter:");
-    console.log(personData.filter (i => i.city === "Malasiqui"));
+console.log(personData.filter((i) => i.city === "Malasiqui"));
 
 console.log("\nmap:");
-    console.log(personData.map(i => `${i.name}  ${i.age}` ));
+console.log(personData.map((i) => `${i.name}  ${i.age}`));
 
 console.log("\nreduce:");
-    console.log(personData.map(i => parseInt(`${i.age}`)).reduce((accumulator, currentValue) => accumulator + currentValue));
+console.log(
+  personData
+    .map((i) => parseInt(`${i.age}`))
+    .reduce((accumulator, currentValue) => accumulator + currentValue)
+);
 
 console.log("\n<------------------------------------->\n");
